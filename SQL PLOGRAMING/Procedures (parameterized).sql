a9da8331ebdc0e968fd0e8ed751b54c9d809bbd8
James Kalenzi <157589595+JEMOJEMOO@users.noreply.github.com>
2026-07-11 18:55:01 +0200

    Add procedures for checking out and returning assets

---
+++ SQL PLOGRAMING/Procedures (parameterized).sql
@@ -0,0 +1,45 @@
+CREATE OR REPLACE PROCEDURE CheckoutAsset
+(
+    p_EmployeeID NUMBER,
+    p_AssetID NUMBER,
+    p_ExpectedReturnDate DATE
+)
+AS
+BEGIN
+    ...
+END CheckoutAsset;
+/
+
+
+
+checkout asset
+
+CREATE OR REPLACE PROCEDURE CheckoutAsset
+(
+    p_EmployeeID NUMBER,
+    p_AssetID NUMBER,
+    p_ExpectedReturnDate DATE
+)
+AS
+BEGIN
+    ...
+END CheckoutAsset;
+/
+
+
+
+
+
+returnasset
+
+
+
+CREATE OR REPLACE PROCEDURE ReturnAsset
+(
+    p_CheckoutID NUMBER
+)
+AS
+BEGIN
+    ...
+END ReturnAsset;
+/
