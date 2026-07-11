c7ebb771f0de96c718d625021a996cfc66718978
James Kalenzi <157589595+JEMOJEMOO@users.noreply.github.com>
2026-07-11 19:01:10 +0200

    Create AssetPackage with asset management procedures

---
+++ SQL PROGRAMMING/package.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE PACKAGE AssetPackage AS
+
+    PROCEDURE CheckoutAsset
+    (
+        p_EmployeeID NUMBER,
+        p_AssetID NUMBER,
+        p_ExpectedReturnDate DATE
+    );
+
+    PROCEDURE ReturnAsset
+    (
+        p_CheckoutID NUMBER
+    );
+
+    FUNCTION TotalBorrowedAssets
+    (
+        p_EmployeeID NUMBER
+    )
+    RETURN NUMBER;
+
+END AssetPackage;
+/
